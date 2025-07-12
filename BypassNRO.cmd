0e906be5f94370bfac2896753e348e47f957bcf6
n00bers00n <n00bers00n@outlook.com>
2025-07-12 16:30:51 -0300

    Update BypassNRO.cmd

--- BypassNRO.cmd
+++ BypassNRO.cmd
@@ -1,5 +1,5 @@
 @echo off
-rem ** Adiciona uma chave de registro para a criação de uma conta local **
+rem ** Adiciona uma chave no registro para a criação de uma conta local **
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v BypassNRO /t REG_DWORD /d 1 /f
 rem ** Reinicia o PC automáticamente ** 
 shutdown /r /t 0
